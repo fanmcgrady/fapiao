cba18b0dedb7948c83bf0d419af09d7eb0c748a4
fangzhiyang <fanmcgrady@gmail.com>
2018-09-28 16:47:30 +0800

    修改debug为false，解决内存泄露

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 ps -ef|grep "runserver 0.0.0.0"|grep -v grep|cut -c 10-15|xargs kill -9
 source activate django
 git pull
-nohup python manage.py runserver 0.0.0.0:8000 &
+nohup python manage.py runserver --insecure 0.0.0.0:8000 &
