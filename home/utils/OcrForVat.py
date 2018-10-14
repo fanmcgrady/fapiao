f4b1a319534be48c0fa47f293ccf4226eaa43f87
Tim21Duncan <297970474@qq.com>
2018-10-14 19:03:33 +0800

    二维码画框最终版

--- home/utils/OcrForVat.py
+++ home/utils/OcrForVat.py
@@ -263,7 +263,7 @@ def init(filepath):
             # 显示二维码
             plt_rects = []
             plt_rects.append(
-                [res[1][0][0] + 5, res[1][0][1] + 5, res[1][0][1] - res[1][1][1] + 10,
+                [res[1][1][0] - 5, res[1][1][1] - 5, res[1][0][1] - res[1][1][1] + 10,
                  res[1][0][1] - res[1][1][1] + 10])
             # 显示
             vis_textline0 = fp.util.visualize.rects(cv2.imread(filepath, 0), plt_rects)
