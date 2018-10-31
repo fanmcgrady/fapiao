3ee839f77d12619c24d6b412293cf41414084283
fangzhiyang <fanmcgrady@gmail.com>
2018-10-31 22:35:12 +0800

    修改newOcr

--- home/utils/OCR/OCR.py
+++ home/utils/OCR/OCR.py
@@ -158,11 +158,3 @@ def OCR(image_path):
     out, _ = predict(image_path, base_model)
 
     return out
-
-
-if __name__ == "__main__":
-    files = os.listdir("/home/huangzheng/ocr/tmp/Image_00002")
-    for i in range(len(files)):
-        test_image = r'/home/huangzheng/ocr/tmp/Image_00002/' + files[i]
-        out = OCR(test_image)
-        print('预测图片为: ' + test_image + '， 预测结果为：', out)
---
+++ home/utils/OCR/Test.py
@@ -0,0 +1,9 @@
+import os
+from home.utils.OCR.OCR import OCR
+
+if __name__ == "__main__":
+    files = os.listdir("/home/huangzheng/ocr/tmp/Image_00002")
+    for i in range(len(files)):
+        test_image = r'/home/huangzheng/ocr/tmp/Image_00002/' + files[i]
+        out = OCR(test_image)
+        print('预测图片为: ' + test_image + '， 预测结果为：', out)
