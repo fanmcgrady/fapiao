ebecd75cf4d2ace606d5d813ebaf66869253d632
fangzhiyang <fanmcgrady@gmail.com>
2018-11-01 07:52:39 +0800

    提前加载模型

--- home/utils/OcrForVat.py
+++ home/utils/OcrForVat.py
@@ -26,10 +26,11 @@ from home import views
 if not views.local_start:
     from scanQRCode.scan_qrcode import recog_qrcode, recog_qrcode_ex
 
+global_model = ocr.load_model()
 
 def jwkj_get_filePath_fileName_fileExt(filename):  # 提取路径
-    (filepath, tempfilename) = os.path.split(filename);
-    (shotname, extension) = os.path.splitext(tempfilename);
+    (filepath, tempfilename) = os.path.split(filename)
+    (shotname, extension) = os.path.splitext(tempfilename)
     return filepath, shotname, extension
 
 def CropPic(filePath, recT, typeT, debug=False, isusebaidu=False):
@@ -60,7 +61,7 @@ def CropPic(filePath, recT, typeT, debug=False, isusebaidu=False):
             if isusebaidu:
                 midResult = flow.OcrPic(sFPN)
             else:
-                midResult = ocr.OCR(sFPN)
+                midResult = ocr.OCR(sFPN, global_model)
             # else:
             #     midResult = OcrNoPic(sFPN)
 
@@ -86,7 +87,7 @@ def CropPic(filePath, recT, typeT, debug=False, isusebaidu=False):
             if isusebaidu:
                 midResult = flow.OcrPic(sFPN)
             else:
-                midResult = ocr.OCR(sFPN)
+                midResult = ocr.OCR(sFPN, global_model)
 
             print('invoiceDateFix: ' + midResult)
             ocrResult['invoiceDate'] = midResult
