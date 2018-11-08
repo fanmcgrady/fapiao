c0d67107b553f96f46a8320f9150fde0e87819c2
fangzhiyang <fanmcgrady@gmail.com>
2018-11-08 10:53:53 +0800

    重构

--- connector/connecter.py
+++ connector/connecter.py
@@ -1,19 +1,19 @@
 # caffe 模型
-import fp.TextBoxes.detect_textline as dt
-global_caffe_model = dt.load_caffe_model()
+# import fp.TextBoxes.detect_textline as dt
+# global_caffe_model = dt.load_caffe_model()
 
-# from OCR.OCR import *
-#
-# global_model = load_model()
-#
-# def OCR(image_path):
-#     """
-#         用来连接OCR调用，通过home/views.py来预加载全局模型
-#         imgae_path 输入图片路径，识别图片为行提取结果
-#     """
-#     global global_model
-#     with K.get_session().graph.as_default():
-#         out, _ = predict(image_path, global_model)
-#
-#     return out
+from OCR.OCR import *
+
+global_model = load_model()
+
+def OCR(image_path):
+    """
+        用来连接OCR调用，通过home/views.py来预加载全局模型
+        imgae_path 输入图片路径，识别图片为行提取结果
+    """
+    global global_model
+    with K.get_session().graph.as_default():
+        out, _ = predict(image_path, global_model)
+
+    return out
 
