bf3cfe50e07bf639980d6072aa80eb52b98a5737
fangzhiyang <fanmcgrady@gmail.com>
2018-11-12 13:05:45 +0800

    清理

--- restart.sh
+++ restart.sh
@@ -4,6 +4,6 @@ ps -ef|grep "runserver 0.0.0.0:8000"|grep -v grep|cut -c 10-15|xargs kill -9
 # 切换到anaconda环境
 source activate python36
 # 更新代码
-git pull
+#git pull
 # 后台启动django服务
 nohup python manage.py runserver 0.0.0.0:8000 --insecure &
