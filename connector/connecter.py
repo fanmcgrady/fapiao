65a9a13106d07bf772d0afe524b8e55012a5051f
tangpeng <tang_peng@126.com>
2018-12-15 07:42:10 +0800

    connecter

--- connector/connecter.py
+++ connector/connecter.py
@@ -1,4 +1,4 @@
-from OCR import OCR as ocrVat
+from OCR import OCR2 as ocrVat
 from OCR import OCRChinese as elecVat
 import keras.backend.tensorflow_backend as K
 
@@ -11,14 +11,14 @@ def OCR(image_path, typeP, attribute, thresholding = 160):
         imgae_path 输入图片路径，识别图片为行提取结果
     """
     global global_model
-    global chinese_global_model
+    # global chinese_global_model
     with K.get_session().graph.as_default():
-        if typeP == 'elec' :
-            print('model:    chinese_global_model')
-            out, _ = elecVat.predict(image_path, chinese_global_model, thresholding=thresholding)
-        else:
-            print('model:    global_model')
-            out, _ = ocrVat.predict(image_path, global_model, thresholding=thresholding)
+        # if typeP == 'elec' :
+        #     print('model:    chinese_global_model')
+        #     out, _ = elecVat.predict(image_path, chinese_global_model, thresholding=thresholding)
+        # else:
+        #     print('model:    global_model')
+        out, _ = ocrVat.predict(image_path, global_model, thresholding=thresholding)
 
     return out
 
